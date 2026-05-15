54f80627cc27e736f01168dac5fd2000197db984
michael-v17 <ms.varela17@gmail.com>
2026-05-14 20:23:56 -0600

    Initial Commit

---
+++ storyplots/storyplotsApp.swift
@@ -0,0 +1,17 @@
+//
+//  storyplotsApp.swift
+//  storyplots
+//
+//  Created by Michael V on 14/5/26.
+//
+
+import SwiftUI
+
+@main
+struct storyplotsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
